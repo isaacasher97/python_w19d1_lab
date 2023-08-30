3333ceff0601c9aea7be93b0575605111fe90a2c
isaacasher97 <isaac.asher97@gmail.com>
2023-08-30 10:02:30 -0400

    Reverse String Completed

---
+++ lab.py
@@ -0,0 +1,19 @@
+## 1. Reverse String
+
+## JS - 
+# function reverseString(str){
+#     return str.split("").reverse().join("")
+# }
+
+# console.log(reverseString("Hello World")) // "dlroW olleH"
+
+## python:
+
+def reverse_string(s):
+    return ''.join(reversed(s))
+
+print(reverse_string("Hello World"))  # Output: "dlroW olleH"
+
+
+
+
