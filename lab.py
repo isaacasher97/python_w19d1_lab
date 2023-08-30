6d5707b093652b40ab19ec78c7a868e585cca6c1
isaacasher97 <isaac.asher97@gmail.com>
2023-08-30 11:52:27 -0400

    random_number completed

--- lab.py
+++ lab.py
@@ -108,3 +108,41 @@ def calculator(num1, num2, operation):
 
 result = calculator(2, 2, "+")
 print(result)  # Output: 4
+
+# 4. Random Number
+
+# JS - 
+# function randomNumber(low, max){
+#     while (true){
+#         const randomNum = Math.floor(Math.random() * max)
+
+#         if(randomNum >= low && randomNum <= max){
+#             return randomNum
+#         }
+#     }
+# }
+
+# console.log(randomNumber(10,20)) //random number between 10 and 20
+
+
+# Python:
+
+import random
+
+def random_number(low, high):
+    while True:
+        random_num = random.randint(low, high)
+
+        if low <= random_num <= high:
+            return random_num
+        
+result = random_number(10, 20)
+print(result) #output: random number between 10 and 20
+
+##Both random.randint(low, high) in Python and Math.floor(Math.random() * max) in JavaScript are used to generate random numbers within a specified range. 
+
+# In python, The random.randint() function in Python is part of the random module which was imported (used instead of Math.random). It directly generates a random integer between low and high instead of low to max. 
+
+# The Python random.randint(low, high) function directly generates an integer within the specified range, which makes it more intuitive and easier to use when you want an integer result.
+
+# In Python's random.randint(low, high), the range is inclusive of both low and high. This means that the random number generated can be equal to either low or high, While In JavaScript's Math.floor(Math.random() * max), the range includes 0 but excludes max. This means the random number generated will be in the range [0, max) – that is, greater than or equal to 0 and strictly less than max.
